a3eb5f4a27b15a2c86c28a180c96268a64325e98
programmingwithVee <basseyvera018@gmail.com>
2023-10-31 16:40:48 +0100

    codar online school

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$("#video1").click(function () {
+    $("video").attr("src", "./video/video.mp4");
+});
+
+$("#video2").click(function () {
+    $("video").attr("src", "./video/video.mp4");
+});
+
+$("#video3").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/9D1x7-2FmTA?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+});
+
+$("#video4").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/pnhO8UaCgxg?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+});
+
+$("#video5").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/0sSYmRImgRY?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+});
+
+$("#video6").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/NbTrGcz4DW8?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+});
+
+$("#video7").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/0XSDAup85SA?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+});
+
+$("#video8").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/4pO-HcG2igk?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+});
+
+$("#video9").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/rb1GWqCJid4?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+});
+
+$("#video10").click(function () {
+    $("video").attr("src", "https://www.youtube.com/embed/tHjxSVaj_wY?list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d");
+});
